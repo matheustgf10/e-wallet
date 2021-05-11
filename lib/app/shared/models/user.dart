7b226cd8c3c7da55cd838c965fd3ffbafb05d238
Matheus Figueiredo <matheustgf64@gmail.com>
2021-05-11 10:31:48 -0300

    Tests and functions in User was added

--- lib/app/shared/models/user.dart
+++ lib/app/shared/models/user.dart
@@ -1,1 +1,66 @@
-class User {}
+import 'dart:ui';
+
+import 'package:ewallet/app/shared/models/account.dart';
+import 'package:flutter/material.dart';
+
+class User {
+  late String idUser;
+  late String name;
+  late String login;
+  late String password;
+  late List<Account> accountList;
+
+  User({
+    required this.idUser,
+    required this.name,
+    required this.login,
+    required this.password,
+    required this.accountList,
+  });
+
+  bool addAccount(String accountName, Color accountColor) {
+    Account account =
+        this.createAccount(name: accountName, color: accountColor);
+
+    this.accountList.add(account);
+
+    return (accountList.isNotEmpty) ? true : false;
+  }
+
+  Account createAccount({String? name, Color? color}) {
+    Account account = Account(
+      idAccount: '',
+      name: name.toString(),
+      color: color!,
+      registerList: [],
+      totalValue: 0,
+      flagAccount: true,
+    );
+
+    return account;
+  }
+
+  Account getAccount({String? idAccount}) {
+    late Account account;
+    account =
+        accountList.firstWhere((element) => element.idAccount == idAccount);
+
+    print(account.runtimeType);
+    return account;
+  }
+
+  bool deleteAccount({String? idAccount}) {
+    dynamic isRemoved = false;
+
+    if (this.checkAccountExists(idAccount: idAccount)) {
+      accountList.removeWhere(isRemoved =
+          (element) => (element.idAccount == idAccount) ? true : false);
+    } else {}
+    return isRemoved;
+  }
+
+  bool checkAccountExists({String? idAccount}) {
+    bool check = accountList.any((element) => element.idAccount == idAccount);
+    return check;
+  }
+}
--- test/app/shared/models/user_test.dart
+++ test/app/shared/models/user_test.dart
@@ -1,5 +1,74 @@
+import 'package:ewallet/app/shared/models/account.dart';
 import 'package:ewallet/app/shared/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class AccountMock extends Mock implements Account {}
 
 void main() {
-  User user = User();
+  late User user;
+  late AccountMock accountMock;
+  late Account account;
+
+  setUpAll(() {
+    user = User(
+        idUser: "HASH_USER",
+        name: "José Silva",
+        login: "jose123",
+        password: "123jose",
+        accountList: [
+          account = Account(
+              idAccount: '123',
+              name: 'Conta 1',
+              color: Colors.amber,
+              registerList: [],
+              totalValue: 99.99,
+              flagAccount: true),
+        ]);
+
+    accountMock = AccountMock();
+  });
+
+  test("O tamanho de accountList deve ser 1", () {
+    expect(user.accountList.length, 1);
+  });
+  test("A função addAccount deve retornar true", () {
+    expect(user.addAccount('Conta 1', Colors.amber), true);
+  });
+
+  test("A função createAccount deve retornar um Account", () {
+    expect(user.createAccount(), AccountMock());
+  });
+
+  group('user.getAccount() Test', () {
+    test(
+        "Se o item existir na lista, a função getAccount deve retornar um Account",
+        () {
+      expect(user.getAccount(idAccount: '123'), account);
+    });
+
+    test(
+        "Se o item não existir na lista, a função getAccount deve retornar um StateError",
+        () {
+      expect(user.getAccount(idAccount: '1234'), isA<Account>());
+    });
+  });
+
+  test(
+      "Se o item não existir na lista, a função deleteAccount deve retornar false",
+      () {
+    expect(user.deleteAccount(idAccount: '1234'), false);
+  });
+  group('user.checkAccountExists Test', () {
+    test("A função checkAccountExists deve retornar true", () {
+      expect(user.checkAccountExists(idAccount: '123'), true);
+    });
+
+    test(
+        "Se o item não existir na lista, a função checkAccountExists deve retornar false",
+        () {
+      expect(user.checkAccountExists(idAccount: '1234'), false);
+    });
+  });
 }
