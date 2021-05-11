7c90903b0d64befa31f5a1efc2546a9f8749d3a6
Lazaro-Barros <36867962+Lazaro-Barros@users.noreply.github.com>
2021-05-11 11:21:50 -0300

    Update Account.dart

--- lib/app/shared/models/account.dart
+++ lib/app/shared/models/account.dart
@@ -1,21 +1,51 @@
-import 'dart:ui';
-
 import 'package:ewallet/app/shared/models/financial_register.dart';
+import 'dart:ui';
+import 'dart:math';
 
 class Account {
-  late String idAccount;
   late String name;
-  late List<FinancialRegister> registerList;
+  final String idAccount = Random.secure().toString();
+  late List<FinancialRegister> financialRegisterList = [];
   late double totalValue;
   late Color color;
   late bool flagAccount;
 
   Account({
-    required this.idAccount,
     required this.name,
-    required this.registerList,
-    required this.totalValue,
     required this.color,
     required this.flagAccount,
   });
+
+  bool createFinancialRegister(
+      {required String description,
+      required double value,
+      required String category,
+      required DateTime date}) {
+    FinancialRegister newFinancialRegister = FinancialRegister(
+      description: description,
+      category: category,
+      value: value,
+      dateRegister: date,
+      idAccount: this.idAccount,
+    );
+    this.financialRegisterList.add(newFinancialRegister);
+    return financialRegisterList.contains(newFinancialRegister);
+  }
+
+  List<FinancialRegister> getAllFinancialRegisters() {
+    return [...financialRegisterList];
+  }
+
+  bool deleteFinancialRegister(String idFinancialRegister){
+    
+    financialRegisterList.removeWhere((element) => element.idFinancialRegister==idFinancialRegister );
+    return true;
+  }
+
+  //bool updateFinancialRegistes() {}
+
+  //FinancialRegister getFinantialRegister(String idFinancialRegister){}
+
+  //double getTotalValue() {}
+
 }
