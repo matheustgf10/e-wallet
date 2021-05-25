e3ce5814650d5d2c08707d1861a734b98cdadc9d
Matheus Figueiredo <matheustgf64@gmail.com>
2021-05-25 14:26:04 -0300

    adding user to appStore

--- lib/app/app_store.dart
+++ lib/app/app_store.dart
@@ -1,3 +1,5 @@
+import 'package:ewallet/app/shared/models/financial_register.dart';
+import 'package:ewallet/app/shared/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 
@@ -8,6 +10,21 @@ class AppStore = AppStoreBase with _$AppStore;
 abstract class AppStoreBase with Store {
   late double height;
   late double width;
+
+  @observable
+  User? user;
+
+  @observable
+  bool haveNewFinancialRegister = false;
+
+  @observable
+  bool hasUpdatedFinancialRegister = false;
+
+  @action
+  bool addFinancialRegister(FinancialRegister financialRegister) {
+    user!.financialRegisterList.add(financialRegister);
+    return true;
+  }
 
   @action
   void setSize({required BuildContext context}) {
--- lib/app/app_store.g.dart
+++ lib/app/app_store.g.dart
@@ -9,7 +9,67 @@ part of 'app_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$AppStore on AppStoreBase, Store {
+  final _$userAtom = Atom(name: 'AppStoreBase.user');
+
+  @override
+  User? get user {
+    _$userAtom.reportRead();
+    return super.user;
+  }
+
+  @override
+  set user(User? value) {
+    _$userAtom.reportWrite(value, super.user, () {
+      super.user = value;
+    });
+  }
+
+  final _$haveNewFinancialRegisterAtom =
+      Atom(name: 'AppStoreBase.haveNewFinancialRegister');
+
+  @override
+  bool get haveNewFinancialRegister {
+    _$haveNewFinancialRegisterAtom.reportRead();
+    return super.haveNewFinancialRegister;
+  }
+
+  @override
+  set haveNewFinancialRegister(bool value) {
+    _$haveNewFinancialRegisterAtom
+        .reportWrite(value, super.haveNewFinancialRegister, () {
+      super.haveNewFinancialRegister = value;
+    });
+  }
+
+  final _$hasUpdatedFinancialRegisterAtom =
+      Atom(name: 'AppStoreBase.hasUpdatedFinancialRegister');
+
+  @override
+  bool get hasUpdatedFinancialRegister {
+    _$hasUpdatedFinancialRegisterAtom.reportRead();
+    return super.hasUpdatedFinancialRegister;
+  }
+
+  @override
+  set hasUpdatedFinancialRegister(bool value) {
+    _$hasUpdatedFinancialRegisterAtom
+        .reportWrite(value, super.hasUpdatedFinancialRegister, () {
+      super.hasUpdatedFinancialRegister = value;
+    });
+  }
+
   final _$AppStoreBaseActionController = ActionController(name: 'AppStoreBase');
+
+  @override
+  bool addFinancialRegister(FinancialRegister financialRegister) {
+    final _$actionInfo = _$AppStoreBaseActionController.startAction(
+        name: 'AppStoreBase.addFinancialRegister');
+    try {
+      return super.addFinancialRegister(financialRegister);
+    } finally {
+      _$AppStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
 
   @override
   void setSize({required BuildContext context}) {
@@ -25,7 +85,9 @@ mixin _$AppStore on AppStoreBase, Store {
   @override
   String toString() {
     return '''
-
+user: ${user},
+haveNewFinancialRegister: ${haveNewFinancialRegister},
+hasUpdatedFinancialRegister: ${hasUpdatedFinancialRegister}
     ''';
   }
 }
