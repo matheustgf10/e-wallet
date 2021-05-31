45df3d5ae93722892d75b8c67315bbd7fa3ca505
Matheus Figueiredo <matheustgf64@gmail.com>
2021-05-31 17:07:50 -0300

    update class to create new financial register

--- lib/app/modules/home/widgets/create_financial_register_widget.dart
+++ lib/app/modules/home/widgets/create_financial_register_widget.dart
@@ -1,17 +1,18 @@
+import 'package:ewallet/app/app_store.dart';
 import 'package:ewallet/app/modules/home/home_store.dart';
-import 'package:ewallet/app/shared/models/account.dart';
+import 'package:ewallet/app/modules/home/widgets/dropdown_widget.dart';
+import 'package:ewallet/app/shared/models/financial_register.dart';
 import 'package:ewallet/app/shared/models/user.dart';
-import 'package:ewallet/app/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class CreateFinancialRegisterWidget extends StatefulWidget {
-  late double height;
-  late double width;
+  AppStore _appStore = Modular.get<AppStore>();
   late User user;
 
-  CreateFinancialRegisterWidget(
-      {required this.height, required this.width, required this.user});
+  CreateFinancialRegisterWidget({
+    required this.user,
+  });
 
   @override
   _CreateFinancialRegisterWidgetState createState() =>
@@ -20,81 +21,93 @@ class CreateFinancialRegisterWidget extends StatefulWidget {
 
 class _CreateFinancialRegisterWidgetState
     extends ModularState<CreateFinancialRegisterWidget, HomeStore> {
-  List<String> convertAccountNamesToList({required List<Account> accounts}) {
-    List<String> listAccountsName = [];
-    int iterable = 0;
-
-    while (iterable < accounts.length) {
-      listAccountsName.add(accounts[iterable].nameAccount);
-      iterable++;
-    }
-
-    return listAccountsName;
-  }
+  TextEditingController _descriptionTextfield = TextEditingController();
+  TextEditingController _valueTextfield = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    String dropdownItem = widget.user.accountList.first.nameAccount;
-    List<String> listAccountNames =
-        convertAccountNamesToList(accounts: widget.user.accountList);
-    return ClipRRect(
-      borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+    return Padding(
+      padding: const EdgeInsets.all(20),
       child: Container(
-        height: widget.height / 2,
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Flexible(
-                  child: TextFormField(
-                decoration: InputDecoration(labelText: 'Descrição'),
-              )),
-              SizedBox(
-                height: 40,
-              ),
-              Flexible(
-                  child: DropdownButton<String>(
-                value: controller.dropdownNewFinancialRegisterValue,
-                icon: const Icon(Icons.arrow_downward),
-                iconSize: 24,
-                elevation: 16,
-                style: const TextStyle(color: PRIMARY_COLOR),
-                underline: Container(height: 2, color: SECONDARY_COLOR),
-                onChanged: (String? newValue) {
-                  controller.dropdownNewFinancialRegisterValue = newValue!;
-                },
-                items: listAccountNames
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              )),
-              SizedBox(
-                height: 40,
-              ),
-              Flexible(
-                  child: TextFormField(
-                decoration: InputDecoration(labelText: 'Conta'),
-              )),
-              SizedBox(
-                height: 40,
-              ),
-              Flexible(
-                child: Container(
-                  alignment: Alignment.bottomRight,
-                  child: ElevatedButton(
-                    child: Text("Adicionar Registro"),
-                    onPressed: () {},
-                  ),
+        height: widget._appStore.height / 2,
+        child: Column(
+          children: [
+            Flexible(
+              child: Container(
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'Descrição'),
+                  controller: _descriptionTextfield,
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Flexible(
+                child: Container(
+              alignment: Alignment.centerLeft,
+              child: DropdownWidget(accountList: widget.user.accountList),
+            )),
+            SizedBox(
+              height: 40,
+            ),
+            Flexible(
+              child: Container(
+                child: TextFormField(
+                  controller: _valueTextfield,
+                  decoration: InputDecoration(labelText: 'Valor'),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment.bottomRight,
+                child: ElevatedButton(
+                  child: Text("Adicionar Registro"),
+                  onPressed: () {
+                    createFinancialRegister();
+
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  // void updateFinancialRegister() {
+  //   FinancialRegister newFinancialRegister = FinancialRegister(
+  //     description: _descriptionTextfield.value.text,
+  //     value: double.parse(_valueTextfield.value.text),
+  //     category: 'COMIDA',
+  //     dateRegister: DateTime.now(),
+  //     idAccount:
+  //         widget._appStore.user!.financialRegisterList[widget.index!].idAccount,
+  //   );
+
+  //   widget._appStore.updateFinancialRegister(
+  //       idFinancialRegister: widget._appStore.user!
+  //           .financialRegisterList[widget.index!].idFinancialRegister,
+  //       editedFinancialRegister: newFinancialRegister);
+  // }
+
+  void createFinancialRegister() {
+    FinancialRegister _newFinancialRegister = FinancialRegister(
+      description: _descriptionTextfield.value.text,
+      value: double.parse(_valueTextfield.value.text),
+      category: 'COMIDA',
+      dateRegister: DateTime.now(),
+      idAccount: widget._appStore.user!.getIdAccountByName(
+          name: controller.dropdownNewFinancialRegisterValue),
+    );
+    widget._appStore
+        .addFinancialRegister(financialRegister: _newFinancialRegister);
   }
 }
