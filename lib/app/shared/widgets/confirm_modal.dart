c2bac0ab74294e3b4c55c929d794b5dedbb4524a
Matheus Figueiredo <matheustgf64@gmail.com>
2021-06-16 14:53:37 -0300

    ConfirmModalWidget has been created

---
+++ lib/app/shared/widgets/confirm_modal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConfirmModalWidget extends StatelessWidget {
+  const ConfirmModalWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
