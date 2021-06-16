3ef8ffd62b52738677b932e251fb6365e38275bf
Matheus Figueiredo <matheustgf64@gmail.com>
2021-06-16 14:52:38 -0300

    adding BACKGROUND_COLOR

--- lib/app/utils/colors.dart
+++ lib/app/utils/colors.dart
@@ -2,3 +2,5 @@ import 'dart:ui';
 
 const PRIMARY_COLOR = Color.fromRGBO(11, 140, 127, 1);
 const SECONDARY_COLOR = Color.fromRGBO(86, 191, 191, 1);
+
+const BACKGROUND_COLOR = Color.fromRGBO(240, 241, 246, 1);
